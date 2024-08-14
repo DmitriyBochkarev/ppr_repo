4bf72f4deca2b3162b07ce95104e15fafc5e5292
Dmitriy <random10208@mail.ru>
2024-08-14 09:28:50 +0300

    add field offer to CandidateCreateView and to task_candidates

--- searchwork/models.py
+++ searchwork/models.py
@@ -18,6 +18,8 @@ class Task(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="client_user")
     worker = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="worker_user")
     status = models.CharField(max_length=200, choices=STATUS_CHOICES, default='Поиск исполнителя')
+    budget = models.DecimalField(max_digits=18, decimal_places=2)
+
     def __str__(self):
         return self.title
 
