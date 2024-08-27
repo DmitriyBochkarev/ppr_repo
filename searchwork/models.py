79a4307c54db4c3a53aa3191fc2c98c679b25247
Dmitriy <random10208@mail.ru>
2024-08-27 21:34:51 +0300

    filter form crispy

--- searchwork/models.py
+++ searchwork/models.py
@@ -31,9 +31,9 @@ class Task(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="client_user")
     worker = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="worker_user")
     status = models.CharField(max_length=200, choices=STATUS_CHOICES, default='Поиск исполнителя')
-    budget = models.DecimalField(max_digits=18, decimal_places=2)
-    type = models.CharField(max_length=200, choices=TYPE_CHOICES, null=False)
-    category = models.CharField(max_length=200, choices=CATEGORY_CHOICES, null=False)
+    budget = models.DecimalField(max_digits=18, decimal_places=2, default=0.00)
+    type = models.CharField(max_length=200, choices=TYPE_CHOICES, null=False, default='Исполнительная документация')
+    category = models.CharField(max_length=200, choices=CATEGORY_CHOICES, null=False, default='Земля')
 
     def __str__(self):
         return self.title
