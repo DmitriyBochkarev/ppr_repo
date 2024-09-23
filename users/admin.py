e81fb18981e6025002aae5c55a2cf17cfb758e6f
Dmitriy <random10208@mail.ru>
2024-09-23 21:50:38 +0300

    form for task_candidate

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Profile, Candidate, Worker, Client
+from .models import Profile, Candidate, Worker, Client, WorkerComment, Conversation, Message, ClientComment, Feedback
 admin.site.register(Profile)
 admin.site.register(Candidate)
 admin.site.register(Worker)
 admin.site.register(Client)
+admin.site.register(WorkerComment)
+admin.site.register(Conversation)
+admin.site.register(Message)
+admin.site.register(ClientComment)
+admin.site.register(Feedback)
